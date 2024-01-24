02c4feb7f8356e02976803c8e5d9664c5fe521e0
DarioDeLaFuente <livet.til.dario@gmail.com>
2024-01-24 22:52:03 +0100

    [FEATURE] Added components/hero banner and sum Function

--- src/Components/hero/HeroBanner.jsx
+++ src/Components/hero/HeroBanner.jsx
@@ -1,29 +1,29 @@
-//* Info:
-//  Add css moduel struktur
-//  + Make the css container to ackt like the jubotron fungjon in botstarap (Canves)
-//*
-
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import styles from './HeroBanner.module.css';
 
 const HeroBanner = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1 className="">Welcome To Our Online Shop</h1>
-          <p>
-            Explore our wide range of products. Find the best deals on the latest items and make your purchase today.
-          </p>
-          <p>
-            <Button variant="primary">Shop Now</Button>
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <div className={styles.heroBanner}>
+        <Row>
+          <Col>
+            <h1 className={styles.heroTitle}>Welcome To Our Online Shop</h1>
+            <p className={styles.heroDescription}>
+              Explore our wide range of products. Find the best deals on the latest items and make your purchase today.
+            </p>
+            <p>
+              <Button variant="primary" className={styles.heroButton}>
+                Shop Now
+              </Button>
+            </p>
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 };
 
--- src/Components/main/Layout.jsx
+++ src/Components/main/Layout.jsx
@@ -1,21 +1,22 @@
-//* Info:
-//  Add css moduel struktur
-//*
-
 import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-import { Outlet } from 'react-router-dom';
-
+import HeroBanner from '../hero/HeroBanner';
+import { Outlet, useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import styles from '../main/Layout.module.css';
 
 const Layout = () => {
+  const location = useLocation();
+
+  const isHomePage = location.pathname === '/';
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <div className={styles.layoutBody}>
       {' '}
       <Header />
+      {isHomePage && <HeroBanner />}
       <Container>
         <Row>
           <Col>
